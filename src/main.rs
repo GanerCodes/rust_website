54684ae95983e2ba1ac9bd9ba58ad2d6fcba649b
Ganer <34655998+GanerCodes@users.noreply.github.com>
2021-11-16 11:36:45 -0600

    asfd

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,17 @@
 mod Rust_AES;
 mod sha256;
 mod server;
+mod config;
 mod utils;
+use crate::config::*;
 use crate::server::*;
 
 use std::mem::drop;
 use std::sync::{Arc, Mutex};
-use std::{thread, str, fs};
+use std::{thread, fs};
 use std::collections::HashMap;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{BufReader, Read, Write, prelude::*};
-
-pub const DOMAIN_NAME: &str = "localhost:234";
-pub const PREFERRED_PROTOCOL: &str = "http";
-pub const AES_KEY: [u8; 16] = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10];
-pub const BASE_DIR: &str = "/Users/Administrator/Documents/Projects/rust_website/files";
-pub const SHORTHAND_FILE_PATH: &str = "URL_shortens.kvm";
-pub const CODES_FILE_PATH: &str = "response_codes.txt";
-pub const MIME_FILE_PATH: &str = "MIME_types.txt";
-pub const UPLOAD_FILE_PATH: &str = "/e/upload";
-pub const ENCRYPTED_PATH_PREFIX: &str = "/e/";
-pub const SHORTHAND_PATH_PREFIX: &str = "/s/";
-pub const WEBSITE_PREFIXES: [&str; 2] = ["http://localhost:234/", "https://localhost:234/"];
-pub const IP_ADDR: &str = "0.0.0.0";
-pub const PORT: u32 = 234;
 
 pub fn main() {
     let mut URL_shorts: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
